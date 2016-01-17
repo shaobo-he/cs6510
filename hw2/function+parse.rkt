269fac86651cd769f1a9c724823b9856df9e6bac
Mark S. Baranowski <mark.s.baranowski@gmail.com>
2016-01-17 15:52:57 -0700

    Seems like working

--- hw2/function+parse.rkt
+++ hw2/function+parse.rkt
@@ -1,6 +1,7 @@
 #lang plai-typed
 
-(define-type-alias args (listof symbol))
+(define-type-alias Args (listof symbol))
+(define-type-alias ExprCs (listof ExprC))
 
 (define-type ExprC
   [numC (n : number)]
@@ -10,11 +11,11 @@
   [multC (l : ExprC)
          (r : ExprC)]
   [appC (s : symbol)
-        (arg : ExprC)])
+        (arg : ExprCs)])
 
 (define-type FunDefC
   [fdC (name : symbol) 
-       (arg : symbol) 
+       (arg : Args) 
        (body : ExprC)])
 
 ;; An expr-S-exp is either
@@ -48,11 +49,31 @@
      (multC (parse (second (s-exp->list s)))
             (parse (third (s-exp->list s))))]
     [(and (s-exp-list? s) 
-          (= 2 (length (s-exp->list s)))
+          (<= 1 (length (s-exp->list s))) ; Application may not require an arg
           (s-exp-symbol? (first (s-exp->list s))))
      (appC (s-exp->symbol (first (s-exp->list s)))
-           (parse (second (s-exp->list s))))]
+           (map parse 
+                (rest (s-exp->list s))))]
     [else (error 'parse "invalid input")]))
+
+(define (contains? [name : symbol] [args : Args]) : boolean
+  (cond
+    [(empty? args) #f]
+    [(cons? args) (or (equal? name (first args))
+                      (contains? name (rest args)))]))
+
+(define (duplicates? [args : Args]) : boolean
+  (cond
+    [(empty? args) #f]
+    [(cons? args) (or (contains? (first args)
+                                 (rest args))
+                      (duplicates? (rest args)))]))
+
+(module+ test
+  (test (duplicates? empty) #f)
+  (test (duplicates? (list 'x)) #f)
+  (test (duplicates? (list 'x 'y)) #f)
+  (test (duplicates? (list 'a 'x 'y 'x 'z)) #t))
 
 (define (parse-fundef [s : s-expression]) : FunDefC
   (cond
@@ -61,11 +82,17 @@
           (s-exp-symbol? (first (s-exp->list s)))
           (eq? 'define (s-exp->symbol (first (s-exp->list s))))
           (s-exp-list? (second (s-exp->list s)))
-          (= 2 (length (s-exp->list (second (s-exp->list s)))))
+          (<= 1 (length (s-exp->list (second (s-exp->list s))))) ; Functions have a name any number of args
           (s-exp-symbol? (first (s-exp->list (second (s-exp->list s)))))
-          (s-exp-symbol? (second (s-exp->list (second (s-exp->list s))))))
+          #;(s-exp-symbol? (second (s-exp->list (second (s-exp->list s)))))
+          )
      (fdC (s-exp->symbol (first (s-exp->list (second (s-exp->list s)))))
-          (s-exp->symbol (second (s-exp->list (second (s-exp->list s)))))
+          (let [(args(map s-exp->symbol 
+               (rest (s-exp->list (second (s-exp->list s))))))]
+            (if (duplicates? args)
+                (error 'parse-func "duplicate argument names")
+                args))
+          ;(s-exp->symbol (second (s-exp->list (second (s-exp->list s)))))
           (parse (third (s-exp->list s))))]
     [else (error 'parse-fundef "invalid input")]))
 
@@ -82,19 +109,35 @@
         (plusC (multC (numC 3) (numC 4))
                (numC 8)))
   (test (parse '{double 9})
-        (appC 'double (numC 9)))
+        (appC 'double (list (numC 9))))
   (test/exn (parse '{{+ 1 2}})
             "invalid input")
 
   (test (parse-fundef '{define {double x} {+ x x}})
-        (fdC 'double 'x (plusC (idC 'x) (idC 'x))))
-  (test/exn (parse-fundef '{def {f x} x})
+        (fdC 'double (list 'x) (plusC (idC 'x) (idC 'x))))
+  (test (parse-fundef '{define {sub x y} {+ x {* -1 y}}})
+        (fdC 'sub (list 'x 'y) (plusC (idC 'x)
+                                      (multC (numC -1) (idC 'y)))))
+  (test (parse-fundef '{define {double2 x} {sub {quadruple x} {double x}}})
+        (fdC 'double2 (list 'x) (appC 'sub (list (appC 'quadruple (list (idC 'x)))
+                                                 (appC 'double (list (idC 'x)))))))
+  (test (parse-fundef '{define {five} 5})
+        (fdC 'five empty (numC 5)))
+  (test/exn (parse-fundef '{def {f (list x)} x})
             "invalid input")
 
   (define double-def
     (parse-fundef '{define {double x} {+ x x}}))
   (define quadruple-def
-    (parse-fundef '{define {quadruple x} {double {double x}}})))
+    (parse-fundef '{define {quadruple x} {double {double x}}}))
+  (define sub-def
+    (parse-fundef '{define {sub x y} {+ x {* -1 y}}}))
+  (define five-def
+    (parse-fundef '{define {five} 5}))
+  (define three-def
+    (parse-fundef '{define {three a b c} {sub {sub a b} {sub b c}}}))
+  (define double2-def
+    (parse-fundef '{define {double2 x} {sub {quadruple x} {double x}}})))
 
 ;; interp ----------------------------------------
 (define (interp [a : ExprC] [fds : (listof FunDefC)]) : number
@@ -103,8 +146,11 @@
     [idC (s) (error 'interp "free variable")]
     [plusC (l r) (+ (interp l fds) (interp r fds))]
     [multC (l r) (* (interp l fds) (interp r fds))]
-    [appC (s arg) (local [(define fd (get-fundef s fds))]
-                    (interp (subst (numC (interp arg fds))
+    [appC (s args) (local [(define fd (get-fundef s fds))]
+                    (interp (subst (map (λ (a)
+                                          (numC (interp a fds)))
+                                        args)
+                             ;(numC (interp args fds))
                                    (fdC-arg fd)
                                    (fdC-body fd))
                             fds))]))
@@ -126,7 +172,23 @@
         16)
   (test (interp (parse '{quadruple 8})
                 (list double-def quadruple-def))
-        32))
+        32)
+  (test (interp (parse '{five}) (list five-def))
+        5)
+  (test (interp (parse '{+ {sub 2 1} 1}) (list sub-def))
+        2)
+  (test (interp (parse '{three 5 1 3}) (list three-def sub-def))
+        6)
+  (test/exn (interp (parse '{three 4 3 2 1}) (list three-def sub-def))
+            "wrong arity") ; too many
+  (test/exn (interp (parse '{three 1 2}) (list three-def sub-def))
+            "wrong arity") ; too few
+  (test (interp (parse '{double2 8})
+                (list double-def double2-def quadruple-def sub-def))
+        16)
+  (test/exn (interp (parse '{add3 5 4 3})
+                    (list (parse-fundef '{define {add3 a b a} '{+ a {+ b a}}})))
+            "duplicate argument names"))
 
 ;; get-fundef ----------------------------------------
 (define (get-fundef [s : symbol] [fds : (listof FunDefC)]) : FunDefC
@@ -149,28 +211,41 @@
             "undefined function"))
 
 ;; subst ----------------------------------------
-(define (subst [what : ExprC] [for : symbol] [in : ExprC])
+(define (subst [what : ExprCs] [for : Args] [in : ExprC]) : ExprC
+  (cond
+    [(and (empty? what) (empty? for)) in]
+    [(and (cons? what) (cons? for))
+     ; =>
+     (subst-one (first what) (first for)
+                (subst (rest what) (rest for) in))]
+    [else (error 'subst "wrong arity")]))
+
+
+;; subst-one ----------------------------------------
+(define (subst-one [what : ExprC] [for : symbol] [in : ExprC]) : ExprC
   (type-case ExprC in
     [numC (n) in]
     [idC (s) (if (eq? for s)
                  what
                  in)]
-    [plusC (l r) (plusC (subst what for l)
-                        (subst what for r))]
-    [multC (l r) (multC (subst what for l)
-                        (subst what for r))]
-    [appC (s arg) (appC s (subst what for arg))]))
+    [plusC (l r) (plusC (subst-one what for l)
+                        (subst-one what for r))]
+    [multC (l r) (multC (subst-one what for l)
+                        (subst-one what for r))]
+    [appC (s args) (appC s (map (λ (a)
+                                  (subst-one what for a))
+                                args))]));(subst what for args))]))
 
 (module+ test
-  (test (subst (parse '8) 'x (parse '9))
+  (test (subst (list (parse '8)) (list 'x) (parse '9))
         (numC 9))
-  (test (subst (parse '8) 'x (parse `x))
+  (test (subst (list (parse '8)) (list 'x) (parse `x))
         (numC 8))
-  (test (subst (parse '8) 'x (parse `y))
+  (test (subst (list (parse '8)) (list 'x) (parse `y))
         (idC 'y))
-  (test (subst (parse '8) 'x (parse '{+ x y}))
+  (test (subst (list (parse '8)) (list 'x) (parse '{+ x y}))
         (parse '{+ 8 y}))
-  (test (subst (parse '8) 'x (parse '{* y x}))
+  (test (subst (list (parse '8)) (list 'x) (parse '{* y x}))
         (parse '{* y 8}))
-  (test (subst (parse '8) 'x (parse '{double x}))
+  (test (subst (list (parse '8)) (list 'x) (parse '{double x}))
         (parse '{double 8})))
